96ad6517f11139b1b393676b5bd0809a1b897ae5
Ricardo Oyarzun <royarzun@gmail.com>
2017-03-14 08:28:24 -0300

    chore(ContentProvider): include content provider in manifest

--- app/src/main/java/info/royarzun/android/somtasks/data/TasksContentProvider.java
+++ app/src/main/java/info/royarzun/android/somtasks/data/TasksContentProvider.java
@@ -7,7 +7,10 @@ import net.simonvt.schematic.annotation.ContentUri;
 import net.simonvt.schematic.annotation.TableEndpoint;
 
 
-@ContentProvider(authority = TasksContentProvider.AUTHORITY, database = SomDatabase.class)
+@ContentProvider(
+        authority = TasksContentProvider.AUTHORITY,
+        database = SomDatabase.class
+)
 public final class TasksContentProvider {
 
     public static final String AUTHORITY = "info.royarzun.android.somtasks.data.TasksContentProvider";
@@ -17,7 +20,7 @@ public final class TasksContentProvider {
 
         @ContentUri(
                 path = "done_tasks",
-                type = "vnd.android.cursor.dir/list",
+                type = "vnd.android.cursor.dir/done_tasks",
                 defaultSort = DoneTaskColumns.TASK_ID + " DESC")
         public static final Uri DONE_TASKS_URI = Uri.parse("content://" + AUTHORITY + "/done_tasks");
     }
@@ -27,7 +30,7 @@ public final class TasksContentProvider {
 
         @ContentUri(
                 path = "pending_tasks",
-                type = "vnd.android.cursor.dir/list",
+                type = "vnd.android.cursor.dir/pending_tasks"    ,
                 defaultSort = PendingTasksColumns.TASK_ID + " DESC")
         public static final Uri PENDING_TASKS_URI = Uri.parse("content://" + AUTHORITY + "/pending_tasks");
     }
